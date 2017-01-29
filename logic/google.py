1cc69ceb2e9dea904888a6950a757635ebae32c9
yomikoyomi <yomi.koyomi@gmail.com>
2017-01-29 11:52:08 +0900

    書き込みできた

--- logic/google.py
+++ logic/google.py
@@ -1,3 +1,6 @@
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
 
@@ -7,5 +10,30 @@ class Google:
     
         return now_date
     
-    def send(self, text):
+    def oauth(self):
+        scope = ['https://spreadsheets.google.com/feeds']
+        path = os.path.expanduser('oauth.json')
+
+        credencials = ServiceAccountCredentials.from_json_keyfile_name(path, scope)
+        client = gspread.authorize(credencials)
+        # gfile = client.open_by_key(doc_id)
+        gfile = client.open('test')
+        worksheet = gfile.sheet1
+        records = worksheet.get_all_values()
+
+        # for record in records:
+        #     print(record)
+
+        return worksheet
+
+    def send(self, oauth, text):
+        now_date = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        worksheet = oauth()
+        num = 3
+        while worksheet.cell(num, 2).value != '':
+            num = num + 1
+        worksheet.update_cell(num, 2, now_date)
+        worksheet.update_cell(num, 3, text)
         return text
+
+
